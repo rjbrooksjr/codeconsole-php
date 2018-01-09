4b83a6c42dcf0a0de28a68891c585830f5ca53e3
rich <rbrooks@gmail.com>
2018-01-08 22:13:37 -0500

    test

--- tests/YourClassTest.php
+++ tests/YourClassTest.php
@@ -6,7 +6,6 @@
 *  For each class in your library, there should be a corresponding Unit-Test for it
 *  Unit-Tests should be as much as possible independent from other test going on.
 *
-*  @author yourname
 */
 class YourClassTest extends PHPUnit_Framework_TestCase{
 	
