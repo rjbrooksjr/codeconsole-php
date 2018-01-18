c79b3f811bf6b4b7d088dafedd994120dcb9e3e8
rich <rbrooks@gmail.com>
2018-01-17 23:02:38 -0500

    psr

--- src/CodeConsole.php
+++ src/CodeConsole.php
@@ -1,26 +1,49 @@
 <?php namespace rjbrooksjr;
 
 use GuzzleHttp\Client;
+use Psr\Log\LogLevel;
 
 class CodeConsole
 {
-    private $client;
+    private static $client = null;
+    private static $apiKey;
 
-    public function __construct()
+    static public function setApiKey($key) 
     {
-        $this->client = new Client([
-            'base_uri' => 'http://localhost',
-            'timeout' => 2,
-        ]);
+        self::$apiKey = $key;
     }
 
-    public function log()
+    static public function log($level = LogLevel::INFO, $message, array $context = [])
     {
-        $params = json_encode(func_get_args());
+        self::send($level, $message, $context);
+    }
 
-        $this->client->post('api/log', [
+    static public function error($message, array $context = [])
+    {
+        self::send(LogLevel::ERROR, $message, $context);
+    }
+
+    static private function send($level, $message, $context)
+    {
+        if (empty(self::$apiKey)) {
+            throw new \Exception('Missing API Key');
+        }
+
+        if (self::$client === null)
+        {
+            self::$client = new Client([
+                'base_uri' => 'http://localhost',
+                'timeout' => 2,
+            ]);
+        }
+
+        array_unshift($context, $message);
+
+        self::$client->post('api/log', [
             'form_params' => [
-                'data' => $params,
+                'key' => self::$apiKey,
+                'type' => $level,
+                'data' => $context,
             ]
         ]);
     }
